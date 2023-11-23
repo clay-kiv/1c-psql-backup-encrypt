c50e216781619b001c3cc495d0f2531e3e42069e
Clay <clay@Clay-PC.kivork.md>
2023-11-23 19:45:09 +0200

    update psql backup script

--- pgpass-backup-encrypt.sh
+++ pgpass-backup-encrypt.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#Ping to cheking start script
+curl -m 10 --retry 5 https://hc-ping.com/gVeC5_elJFMwq-RQ8pDWbA/1c-backup-psql-dump/start
 
 set -e
 
@@ -7,6 +9,7 @@ PSQL_USER=postgres
 BACKUP_DIR="/root/backup-psql"
 GPG_EMAIL=vladimir@kiv.md
 GDRIVE_PATH="backup-google:/1cdb/psql_backups-1cdb"
+DATE=$(date +%Y_%m_%d-%H_%M)
 
 
 DATABASES=$(psql -h localhost -U postgres -l -t -w | cut -d '|' -f 1 | sed -e 's/^[[:space:]]*//' | grep -v -E "postgres|template0|template1")
@@ -14,11 +17,14 @@ DATABASES=$(psql -h localhost -U postgres -l -t -w | cut -d '|' -f 1 | sed -e 's
 
 for DB in $DATABASES
 do
-    pg_dump -h $PSQL_HOST -U $PSQL_USER -d $DB | gzip -c > $BACKUP_DIR/$DB.sql.gz
+    pg_dump -h $PSQL_HOST -U $PSQL_USER -v -d $DB | gzip -c > $BACKUP_DIR/$DB.sql.gz
 
-    gpg --encrypt -r $GPG_EMAIL --batch --yes --trust-model always -o $BACKUP_DIR/$DB.sql.gz.gpg - < $BACKUP_DIR/$DB.sql.gz && rm -rf $BACKUP_DIR/$DB.sql.gz
+    gpg --encrypt -r $GPG_EMAIL --batch --yes --trust-model always -o $BACKUP_DIR/$DB-$DATE.sql.gz.gpg - < $BACKUP_DIR/$DB.sql.gz && rm -rf $BACKUP_DIR/$DB.sql.gz
 
-    rclone copy -P $BACKUP_DIR/$DB.sql.gz.gpg $GDRIVE_PATH/$DB_$(date +%Y_%m_%d-%H_%M).sql.gz.gpg && rm -rf $BACKUP_DIR/$DB.sql.gz.gpg
+    rclone copy -P $BACKUP_DIR/$DB-$DATE.sql.gz.gpg $GDRIVE_PATH/$(date +%Y_%m_%d) && rm -rf $BACKUP_DIR/$DB-$DATE.sql.gz.gpg
 done
+
+#Ping to cheking if script finished backup.
+curl -m 10 --retry 5 https://hc-ping.com/gVeC5_elJFMwq-RQ8pDWbA/1c-backup-psql-dump
 
 echo "Backups completed!"
